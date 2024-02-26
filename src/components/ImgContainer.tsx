67f33bb64ace484ea5c686079be8006e2fa545c8
sytnikov <sytnikov.alexey@gmail.com>
2024-02-26 12:56:36 +0200

    image container component added, picture display

---
+++ src/components/ImgContainer.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+import { Photo } from "@/models/Images";
+
+type Props = {
+  photo: Photo;
+};
+
+export default function ImgContainer({ photo }: Props) {
+  return (
+    <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
+      <Image 
+        src={photo.src.large}
+        alt={photo.alt}
+        width={250}
+        height={250}
+      />
+    </div>
+  );
+}
